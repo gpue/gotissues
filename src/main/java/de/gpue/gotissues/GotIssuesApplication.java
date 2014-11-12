176448c9ddec327871bfac46cb8bd4a26df7bc97
gpue <gpue@tablet>
2014-11-12 22:29:09 +0100

    fixed pom.xml for war deployment

--- src/main/java/de/gpue/gotissues/GotIssuesApplication.java
+++ src/main/java/de/gpue/gotissues/GotIssuesApplication.java
@@ -4,13 +4,15 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-public class GotIssuesApplication {
+public class GotIssuesApplication extends SpringBootServletInitializer{
 
 	private static final Log log = LogFactory
 			.getLog(GotIssuesApplication.class);
@@ -19,5 +21,10 @@ public class GotIssuesApplication {
 		log.debug("Starting...");
 		SpringApplication.run(GotIssuesApplication.class, args);
 	}
+	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(getClass());
+    }
 
 }
